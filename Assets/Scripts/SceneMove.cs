395bfb6c3c5e63c495ad0864c7df9280ae66901d
SamuelBamuel <samdinardi@gmail.com>
2025-09-27 04:06:31 -0400

    scene traversal

--- Assets/Scripts/SceneMove.cs
+++ Assets/Scripts/SceneMove.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SceneMove : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Camera scene1Camera;
+    public Camera scene2Camera;
     void Start()
     {
         
@@ -13,6 +15,15 @@ public class SceneMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            scene1Camera.enabled = true;
+            scene2Camera.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            scene1Camera.enabled = false;
+            scene2Camera.enabled = true;
+        }
     }
 }
